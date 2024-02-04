d6bc2b952656cdd703b142d604d39d8b84b4acf4
camillewu90 <51872168+camillewu90@users.noreply.github.com>
2024-02-03 18:46:55 -0800

    Add a table header and rows

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
@@ -15,12 +14,40 @@ for filepath in filepaths:
     invoice_nr, date = filename.split("-")
 
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=50, h=8, border=0, txt=f"Invoice nr.{invoice_nr}",
-             ln=1)
+    pdf.cell(w=50, h=8, border=0, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=50, h=8, border=0, txt=f"Date: {date}",
-             ln=1)
+    pdf.cell(w=50, h=8, border=0, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = list(df.columns)
+    columns = [column.replace("_", " ").title() for column in columns]
+
+    pdf.set_font(family='Times', style='B', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, border=1, txt=columns[0])
+    pdf.cell(w=70, h=8, border=1, txt=columns[1])
+    pdf.cell(w=35, h=8, border=1, txt=columns[2])
+    pdf.cell(w=30, h=8, border=1, txt=columns[3])
+    pdf.cell(w=30, h=8, border=1, txt=columns[4], ln=1)
+
+    # Add roes to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family='Times', size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, border=1, txt=str(row.product_id))
+        pdf.cell(w=70, h=8,border=1, txt=str(row.product_name))
+        pdf.cell(w=35, h=8, border=1, txt=str(row.amount_purchased))
+        pdf.cell(w=30, h=8, border=1, txt=str(row.price_per_unit))
+        pdf.cell(w=30, h=8, border=1, txt=str(row.total_price), ln=1)
+
+    """for column in df.columns:
+        column = column.replace("_", " ").title()
+        print(column)
+        pdf.set_font(family='Times', style='B', size=16)
+        pdf.cell(w=30, h=8, border=1, txt=f"{column}")"""
 
     pdf.output(f"PDFs/{filename}.pdf")
 
